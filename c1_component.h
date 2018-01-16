78f82c277c92560992a0d38ec78a11f0a1f7c271
Bakouchine <mbakouchine@vmlxhi-070.adit-jv.com>
2018-01-16 17:08:37 +0100

    Static library implementation, baseline

---
+++ c1_component.h
@@ -0,0 +1,13 @@
+/*
+ * Declaration of public API for software component C1
+ */
+
+// includes
+#include <stdint.h>
+
+// API
+int32_t init (void);
+int32_t terminate (void);
+
+int32_t start (void);
+int32_t stop (void);
